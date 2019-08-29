abb6662a7c9dd64580117b38415518e5a360e561
Linda Puzey <47217463+lpuzey@users.noreply.github.com>
2019-08-29 15:57:44 -0400

    Started add an array for the heuristic values
    I haven't been able to test this, but I should pick the middle option because it has the highest value in the heuristic array

--- CS4341_Tournament/src/Players/NewPlayer.java
+++ CS4341_Tournament/src/Players/NewPlayer.java
@@ -5,6 +5,8 @@ import Utilities.StateTree;
 
 public class NewPlayer extends Player{
 
+	int turnNum = 0;
+	
 	public NewPlayer(String n, int t, int l) {
 		super(n, t, l);
 		// TODO Auto-generated constructor stub
@@ -12,26 +14,37 @@ public class NewPlayer extends Player{
 
 	@Override
 	public Move getMove(StateTree state) {
+		//array with all are heuristic evaluations for each column
+		//The size of the array is equal to the number of columns in the board
+		int[] hueristicEval = new int[state.columns];
+		
+		//Goes through the array and makes everything 0 unless it is the middle element in which it makes it 1
 		for(int j=0; j<state.columns; j++)
 		{
-			for(int i=0; i<state.rows; i++)
-			{
-				//(0 = empty, 1 = player1, 2 = player2)
-				//if the row and column is empty
-				if(state.getBoardMatrix()[i][j] == 0)
-				{
-					if((this.turn == 1) || (this.turn == 2))
-						return new Move(false, (state.columns-1)/ 2);	
-					else
-						return new Move(false, j+1); // j is the column number starting at 0
-				}
-				//starting the game we want them to take and control the middle of the board			
-
+			//checks if it is the middle term
+			if(j==(state.columns-1)/ 2) {
+				//makes it equal to 1 because that is most valuable at the beginning of the game. 
+				hueristicEval[j] = 1;
 			}
-			
+			else
+				hueristicEval[j] = 0;
 		}
-		return new Move(false, 100);
+
+		int max = hueristicEval[0];
+		int index = -1000;
+
+		//goes through the list looking for the maximum value and finds the index of it
+		for (int i = 0; i < hueristicEval.length; i++) 
+		{
+			if (max < hueristicEval[i]) 
+			{
+				max = hueristicEval[i];
+				index = i;
+			}
+		}
+		//plays the move with the highest heuristic value
+		return new Move(false, index);	
+			
 	}
-	
 
 }
