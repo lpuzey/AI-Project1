59fc34915d294f5e99bdf93bab0cdbf931c722c0
Linda Puzey <47217463+lpuzey@users.noreply.github.com>
2019-08-29 10:00:27 -0400

    I think it is doing something
    I tried to get it to take the middle move when it starts off, as that is the most valuable. I thought it wasn't working, but it kinda is

--- CS4341_Tournament/src/Players/NewPlayer.java
+++ CS4341_Tournament/src/Players/NewPlayer.java
@@ -12,8 +12,25 @@ public class NewPlayer extends Player{
 
 	@Override
 	public Move getMove(StateTree state) {
-		// TODO Auto-generated method stub
-		return null;
+		for(int j=0; j<state.columns; j++)
+		{
+			for(int i=0; i<state.rows; i++)
+			{
+				//(0 = empty, 1 = player1, 2 = player2)
+				//if the row and column is empty
+				if(state.getBoardMatrix()[i][j] == 0)
+				{
+					return new Move(false, j); // j is the column number starting at 0
+				}
+				//starting the game we want them to take and control the middle of the board			
+				if(this.turn == 1)
+					return new Move(false, (state.columns+1)/ 2);
+				if(this.turn == 2)
+					return new Move(false, (state.columns+1)/2);	
+			}
+			
+		}
+		return new Move(false, 100);
 	}
 	
 
