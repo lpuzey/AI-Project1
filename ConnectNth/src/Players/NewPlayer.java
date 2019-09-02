8c3f09e46376395203984bcdede9380be2e9fb15
Linda Puzey <47217463+lpuzey@users.noreply.github.com>
2019-09-01 20:34:18 -0400

    Added a lot of hueristic functions
    -Need to finsh score function
    -Need to make sure it doesn't try to put in a row that is full
    -Need to work it in with the minimax stuff

--- ConnectNth/src/Players/NewPlayer.java
+++ ConnectNth/src/Players/NewPlayer.java
@@ -1,5 +1,8 @@
 package Players;
 
+
+import java.util.Arrays;
+
 import Utilities.Move;
 import Utilities.StateTree;
 
@@ -14,7 +17,6 @@ public class NewPlayer extends Player{
 
 	@Override
 	public Move getMove(StateTree state) {
-		System.out.println("hello");
 		//array with all are heuristic evaluations for each column
 		//The size of the array is equal to the number of columns in the board
 		int[] hueristicEval = new int[state.columns];
@@ -36,16 +38,22 @@ public class NewPlayer extends Player{
 		
 		for(int j=0; j<state.columns; j++)
 		{
-			for(int i=0; i<state.rows; i++)
+			for(int i=0; i<state.rows - 3; i++)
 			{
 				if((turn == 2)&& (state.getBoardMatrix()[i][j] == 1)&&(state.getBoardMatrix()[i+1][j] == 1)&&(state.getBoardMatrix()[i+2][j]==1)) {
 					
 					hueristicEval[j] = 2;
-					System.out.println(hueristicEval[j]);
+					if(state.getBoardMatrix()[i+3][j]==2) {
+						hueristicEval[j] = 0;
+					}
 				}
 				
 			}
 		}
+		if(winningColumn(state,turn) != -1) {
+			hueristicEval[winningColumn(state,turn)] += 1000;
+		}
+		
 		
 		//goes through the list looking for the maximum value and finds the index of it
 		for (int i = 0; i < hueristicEval.length; i++) 
@@ -57,32 +65,127 @@ public class NewPlayer extends Player{
 			}
 		}
 		
-		
-		
-		
-
-		
-		
 
 		//plays the move with the highest heuristic value
 	//	if(state.getBoardMatrix()[i][index] == 0)
 	//	{
-		StateTree test = StateTree(1, 2, 3, 4, true, false, state);
-		state.children.add(test);
+	//	StateTree test = StateTree(1, 2, 3, 4, true, false, state);
+	//state.children.add(test);
 		turnNum++;
-		System.out.println("MINIMAX: " + minimax(state, turnNum, max));
+		
+//		System.out.println("MINIMAX: " + minimax(state, turnNum, max));
+			
 			return new Move(false, index);	
 			
-	//	}
+		}
 		
 			
+	int evaluateScore(StateTree state, int piece) {
+		//horizontal score
+		int score = 0;
+		int[] rowArray = new int[state.rows];
+		int[] columnArray = new int[state.columns];
+		int winNum = state.winNumber - 1;
+
+		// score Horizontal
+		for(int r=0; r<state.rows; r++){
+			rowArray[r] = r;
+			for(int c=0; c<state.columns- winNum; c++){
+				int[] window = Arrays.copyOfRange(rowArray, c , c + winNum );
+				//count the number of piece and see if it is equal to state.winNumber
+				score += 100;
+				//count num of piece in window and see if it is equal to 3 and one of them is empty == 0
+				//might be able to use winning column
+				score += 10;	
+			}
+		}
+		//score Vertical
+		for(int c=0; c<state.columns; c++){
+			columnArray[c] = c;
+			for(int r=0; r<state.rows- winNum; r++){
+				int[] window = Arrays.copyOfRange(columnArray, r , r + winNum );
+				//count the number of piece in window and see if it is equal to state.winNumber
+				score += 100;
+				//count num of piece and see if it is equal to 3 and one of them is empty == 0
+				//might be able to use winning column
+				score += 10;	
+			}
+		}
+		//upwards diagonals
+		for(int r=0; r<state.rows - winNum; r++){
+			for(int c=0; c<state.columns- winNum; c++){
+			//	int[] window = 
+				//count the number of piece and see if it is equal to state.winNumber
+				score += 100;
+				//count num of piece in window and see if it is equal to 3 and one of them is empty == 0
+				//might be able to use winning column
+				score += 10;	
+			}
+		}
+		
+		return score;
+		
 	}
+	
+	//returns the Column number for a winning 4 in a row
+	int winningColumn(StateTree state, int piece){ 
+		int winNum = state.winNumber - 1;
+		//check Horizontal
+		for(int c=0; c<state.columns - winNum; c++)
+		{
+			for(int r=0; r<state.rows; r++)
+			{
+				if((state.getBoardMatrix()[r][c] == piece)&&(state.getBoardMatrix()[r][c+1] == piece)&&(state.getBoardMatrix()[r][c+2] == piece)&&(state.getBoardMatrix()[r][c+3] == 0)) {	
+					return c+3; 
+				}
+					
+			}
+		}
+		//check Vertical
+		for(int c=0; c<state.columns; c++)
+		{
+			for(int r=0; r<state.rows - winNum; r++)
+			{
+				if((state.getBoardMatrix()[r][c] == piece)&&(state.getBoardMatrix()[r+1][c] == piece)&&(state.getBoardMatrix()[r+2][c] == piece)&&(state.getBoardMatrix()[r+3][c] == 0)) {
+					return c; 
+				}
+					
+			}
+		}
+		//check diagonals going up
+		for(int c=0; c<state.columns - winNum; c++)
+		{
+			for(int r=0; r<state.rows - winNum; r++)
+			{
+				if((state.getBoardMatrix()[r][c] == piece)&&(state.getBoardMatrix()[r+1][c+1] == piece)&&(state.getBoardMatrix()[r+2][c+2] == piece)&&(state.getBoardMatrix()[r+3][c+3] == 0)) {
+					return c+3; 
+				}
+					
+			}
+		}
+		//check diagonals going down
+		for(int c=0; c<state.columns - winNum; c++)
+		{
+			for(int r=winNum; r<state.rows ; r++)
+			{
+				if((state.getBoardMatrix()[r][c] == piece)&&(state.getBoardMatrix()[r-1][c+1] == piece)&&(state.getBoardMatrix()[r-2][c+2] == piece)&&(state.getBoardMatrix()[r-3][c+3] == 0)) {
+					return c+3; 
+				}
+					
+			}
+		}
+		return -1;
+	}
+		 
+	
+
+
 	
 	/**
 	* This function will create children for any element in a tree that doent have children
 	* and isn't and end condition
 	*/
-	public void makeChildren(StateTree state) 
+/*	public void makeChildren(StateTree state) 
 	{
 		if(state.children.isEmpty())  // if this statetree object has no children, make it children
 		{
@@ -132,5 +235,6 @@ public class NewPlayer extends Player{
 		  return eval;
 		 }
 		}
-
+*/
 }
+
